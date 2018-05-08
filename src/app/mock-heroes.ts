c979fc4ef6993fec1b75b24fbbb5488f3f09f600
santiagojprado@gmail.com <santiagojprado@gmail.com>
2018-05-08 00:45:00 -0300

    Add services

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -2,6 +2,7 @@ import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
+  { id: 1, name: 'Mr. rabino' },
   { id: 12, name: 'Narco' },
   { id: 13, name: 'Bombasto' },
   { id: 14, name: 'Celeritas' },
